49bf6c62459e47f953d9cb7310960864c19f68b9
jenifferwu <yywu@szucla.org>
2017-06-15 16:15:23 +0800

    Modify generation caffe train file programs

--- caffe_CNN_training/generate_caffe_train_file.py
+++ caffe_CNN_training/generate_caffe_train_file.py
@@ -14,18 +14,41 @@ TIANCHI_train_seriesuids = TIANCHI_train_path + "seriesuids.csv"
 output_path = "/home/jenifferwu/Caffe_CNN_Data/"
 train_file = "train.txt"
 
+csvRows = []
+
+
+#####################
+def csv_row(seriesuid, diameter_mm, nodule_class):
+    new_row = []
+    new_row.append(seriesuid)
+    new_row.append(diameter_mm)
+    new_row.append(nodule_class)
+    csvRows.append(new_row)
+
+
+def is_nodule(diameter_mm):
+    nodule_class = 0
+    # print float(diameter_mm)
+    # print float(diameter_mm) >= 10
+    if float(diameter_mm) >= 10:
+        nodule_class = 1
+    return nodule_class
+
+
 #####################
 
 # Read the annotations CSV file in (skipping first row).
-csvRows = []
+
 csvFileObj = open(TIANCHI_train_annotations)
-readerObj = csv.reader(csvFileObj)
+readerObj = csv.DictReader(csvFileObj)
+
+csv_row('seriesuid', 'diameter_mm', 'nodule_class')
 for row in readerObj:
     if readerObj.line_num == 1:
-        continue    # skip first row
-    csvRows.append(row)
-    # csvRows.append(row['seriesuid'])
-    # csvRows.append(row['diameter_mm'])
+        continue  # skip first row
+
+    csv_row(row['seriesuid'], row['diameter_mm'], is_nodule(row['diameter_mm']))
+
 csvFileObj.close()
 
 
