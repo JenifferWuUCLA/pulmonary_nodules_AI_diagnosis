8db298a3357dfe23bc6634f9d9b6b7621399e39b
jenifferwu <yywu@szucla.org>
2017-06-16 10:18:53 +0800

    Pre-processed Images in the specified directory

--- mask_segment/LUNA_segment_lung_ROI_preprocessed.py
+++ mask_segment/LUNA_segment_lung_ROI_preprocessed.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# TIANCHI_segment_lung_ROI.py - Pre-processed Images with region of interest in lung
+# LUNA_segment_lung_ROI_preprocessed.py - Pre-processed Images with region of interest in lung
 
 import numpy as np
 from skimage import morphology
@@ -10,16 +10,14 @@ from glob import glob
 import cv2
 import os
 
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-
-subset = "train_subset00/"
-working_path = "/home/jenifferwu/Caffe_CNN_Data/IMAGE_MASKS_DATA/" + subset
-file_list=glob(working_path+"images_*.npy")
+working_path = "/home/jenifferwu/IMAGE_MASKS_DATA/"
+file_list = glob(working_path + "images_*.npy")
 
 for img_file in file_list:
     imgs_to_process = np.load(img_file).astype(np.float64)
-    fig = plt.figure(figsize=(12,4))
+    # fig = plt.figure(figsize=(12,4))
     '''
     for i in range(3):
         ax = fig.add_subplot(1,3,i+1)
@@ -29,29 +27,29 @@ for img_file in file_list:
 
     for i in range(len(imgs_to_process)):
         img = imgs_to_process[i]
-        #Standardize the pixel values
+        # Standardize the pixel values
         mean = np.mean(img)
         std = np.std(img)
-        img = img-mean
-        img = img/std
+        img = img - mean
+        img = img / std
         # Find the average pixel value near the lungs
-        #　to renormalize washed out images
-        middle = img[100:400,100:400]
+        # 　to renormalize washed out images
+        middle = img[100:400, 100:400]
         mean = np.mean(middle)
         max = np.max(img)
         min = np.min(img)
         # To improve threshold finding, I'm moving the
-        #　underflow and overflow on the pixel spectrum
-        img[img==max]=mean
-        img[img==min]=mean
+        # 　underflow and overflow on the pixel spectrum
+        img[img == max] = mean
+        img[img == min] = mean
         # Using Kmeans to separate foreground (radio-opaque tissue)
-        #　and background (radio transparent tissue ie lungs)
+        # 　and background (radio transparent tissue ie lungs)
         # Doing this only on the center of the image to avoid
-        #　the non-tissue parts of the image as much as possible
-        kmeans = KMeans(n_clusters=2).fit(np.reshape(middle,[np.prod(middle.shape),1]))
+        # 　the non-tissue parts of the image as much as possible
+        kmeans = KMeans(n_clusters=2).fit(np.reshape(middle, [np.prod(middle.shape), 1]))
         centers = sorted(kmeans.cluster_centers_.flatten())
         threshold = np.mean(centers)
-        thresh_img = np.where(img<threshold,1.0,0.0) # threshold the image
+        thresh_img = np.where(img < threshold, 1.0, 0.0)  # threshold the image
 
         # 对一张图中所有像素点的亮度做概率密度分布 , 用竖线标注阈值所在
         '''
@@ -72,7 +70,7 @@ for img_file in file_list:
         '''
 
         # 增大黑色部分(非 ROI )的区域,使之尽可能的连在一起
-        eroded = morphology.erosion(thresh_img,np.ones([4,4]))
+        eroded = morphology.erosion(thresh_img, np.ones([4, 4]))
         '''
         ax3 = fig.add_subplot(323)
         ax3.imshow(eroded, "gray")
@@ -81,7 +79,7 @@ for img_file in file_list:
         '''
 
         # 增大白色部分( ROI )的区域,尽可能的消除面积较小的黑色区域
-        dilation = morphology.dilation(eroded,np.ones([10,10]))
+        dilation = morphology.dilation(eroded, np.ones([10, 10]))
         '''
         ax4 = fig.add_subplot(324)
         ax4.imshow(dilation, "gray")
@@ -107,14 +105,14 @@ for img_file in file_list:
         good_labels = []
         for prop in regions:
             B = prop.bbox
-            if B[2]-B[0]<475 and B[3]-B[1]<475 and B[0]>40 and B[2]<472:
+            if B[2] - B[0] < 475 and B[3] - B[1] < 475 and B[0] > 40 and B[2] < 472:
                 good_labels.append(prop.label)
 
         mask = np.zeros_like(labels)
         for N in good_labels:
-            mask = mask + np.where(labels==N,1,0)
+            mask = mask + np.where(labels == N, 1, 0)
 
-        mask = morphology.dilation(mask,np.ones([10,10])) # one last dilation
+        mask = morphology.dilation(mask, np.ones([10, 10]))  # one last dilation
 
         '''
         ax6 = fig.add_subplot(326)
